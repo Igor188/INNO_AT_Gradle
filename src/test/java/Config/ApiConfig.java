7052f0ba5e30e4b8c98802b9ab7e03b41865547d
Igor188 <igor_turdakov@mail.ru>
2026-09-13 23:02:30 +0400

    Lesson8HomeworkConfiguration

--- src/test/java/Config/ApiConfig.java
+++ src/test/java/Config/ApiConfig.java
@@ -12,11 +12,30 @@ Config.LoadType.MERGE
 
 public interface ApiConfig extends Config {
     @Key("URL")
-    public String url();
+    String url();
 
-    @Key("MODE")
-    public String mode();
+    @Key("API_URL")
+    String apiUrl();
+
+    @Key("TIMEOUT")
+    @DefaultValue("10000")
+    int timeout();
+
+    @Key("API_LOGS")
+    @DefaultValue("TRUE")
+    boolean apiLogs();;
+
+    @Key("ADMIN_LOGIN")
+    String adminLogin();
+
+    @Key("ADMIN_PASSWORD")
+    String adminPassword();
 
     @Key("GOOD_NAME")
-    public String GOOD_NAME();
+    @DefaultValue("BASE_GOOD_NAME")
+    String goodName();
+
+    @Key("GOOD_PRICE")
+    @DefaultValue("50.0")
+    double goodPrice();
 }
